320220623685eeb6c93fe55f3c8986afde781eec
Cra2yJoker <charlieting.um@gmail.com>
2021-09-18 23:45:33 +0800

    clean up

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const MONGODB_URL = process.env.MONGODB_CONNECTION || 'mongodb://127.0.0.1:27017
 
 const express = require("express");
 const app = express();
-// const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const appointmentRoutes = require('./api/routes/appointment');
 
-mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
-
-// app.use(morgan("dev"));
-// app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
